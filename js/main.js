07e52a05871d8291d9b6f7afa7ede159d6a69816
chachamin <wlauswlaus@hanmail.net>
2024-08-02 17:34:27 +0900

    빠른예약

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-$(document).ready(function(){
-	$.ajax({
-        url:'/main/familySite.do?mod=webMain',
-        type:'post',
-        datatype: 'json',
-        error:function(error,status,e){
-               alert('통신에 실패 하였습니다.');
-        },
-        success: function(data){        	
-        	var obj = eval("("+data+")");  
-	        	$.each(obj, function (index, value) {   
-	        		if(value.PRO_CG == "TNT1"){
-	        			$("#tnt1").append("<li><a href="+value.PRO_LINK+"><span>"+value.PRO_NAME+"</span></a></li>");
-	        		}else{
-	        			$("#tnt2").append("<li><a href="+value.PRO_LINK+"><span>"+value.PRO_NAME+"</span></a></li>");
-	        		}
-	        		//console.log(value.PRO_NAME);    		        	
-	            });	  
+// $(document).ready(function(){
+// 	$.ajax({
+//         url:'/main/familySite.do?mod=webMain',
+//         type:'post',
+//         datatype: 'json',
+//         error:function(error,status,e){
+//                alert('통신에 실패 하였습니다.');
+//         },
+//         success: function(data){        	
+//         	var obj = eval("("+data+")");  
+// 	        	$.each(obj, function (index, value) {   
+// 	        		if(value.PRO_CG == "TNT1"){
+// 	        			$("#tnt1").append("<li><a href="+value.PRO_LINK+"><span>"+value.PRO_NAME+"</span></a></li>");
+// 	        		}else{
+// 	        			$("#tnt2").append("<li><a href="+value.PRO_LINK+"><span>"+value.PRO_NAME+"</span></a></li>");
+// 	        		}
+// 	        		//console.log(value.PRO_NAME);    		        	
+// 	            });	  
         	
-        }
-	});	
-});
+//         }
+// 	});	
+// });
